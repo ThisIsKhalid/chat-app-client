2f60fce8d9a26cd76c20b71624c703f051cf3618
ThisIsKhalid <iamkhalid.hs@gmail.com>
2024-10-08 10:37:11 +0600

    chat ui update

--- src/app/chat/[orderId]/page.tsx
+++ src/app/chat/[orderId]/page.tsx
@@ -166,8 +166,8 @@ export default function ChatPage({ params }: { params: { orderId: string } }) {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col text-[#433443]">
-      <div className="bg-white p-4 shadow-md flex items-center justify-between">
+    <div className="min-h-screen bg-gray-100 flex flex-col text-[#433443] relative">
+      <div className="fixed w-full z-50 bg-white p-4 shadow-md flex items-center justify-between">
         <div className="flex items-center gap-4">
           <MdArrowBackIosNew size={20} />
           <div className="flex gap-2 items-center">
@@ -191,7 +191,7 @@ export default function ChatPage({ params }: { params: { orderId: string } }) {
             <div>
               <h1 className="font-bold text-lg">
                 {senderRole === "CUSTOMER"
-                  ? chat?.helper?.firstName + " " + chat?.helper?.lastName
+                  ? chat?.customer?.firstName + " " + chat?.customer?.lastName
                   : chat?.helper?.firstName + " " + chat?.helper?.lastName}
               </h1>
               <h1 className="text-gray-500 text-sm">
@@ -217,7 +217,9 @@ export default function ChatPage({ params }: { params: { orderId: string } }) {
       </div>
 
       {messages.length > 0 ? (
-        <div className={`flex flex-col  p-4 overflow-y-auto`}>
+        <div
+          className={`container mx-auto transition-all duration-300 ease-in-out flex flex-col  p-4 overflow-y-auto`}
+        >
           {messages.map((message) => (
             <div
               key={message.id}
@@ -242,19 +244,19 @@ export default function ChatPage({ params }: { params: { orderId: string } }) {
           No messages yet
         </div>
       )}
-      <div className="p-4 bg-white flex items-center">
+      <div className="p-4 bg-white flex items-center flex-col w-full gap-2">
         <textarea
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
           onKeyPress={handleTyping}
           placeholder="Type your message..."
-          className="flex-1 border rounded-lg p-2"
+          className="flex-1 border rounded-3xl px-6 py-3 w-full"
         />
         <button
           onClick={handleSendMessage}
-          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg"
+          className="ml-2 px-6 py-3 bg-[#4D88D5] text-white rounded-3xl w-full"
         >
-          Send
+          Send Message
         </button>
       </div>
     </div>
